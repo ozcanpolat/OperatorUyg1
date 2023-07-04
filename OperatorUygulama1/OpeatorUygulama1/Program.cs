8f599723612157f6f2dab4fead564e352e10c057
ozcanpolat <93453642+ozcanpolat@users.noreply.github.com>
2023-07-04 14:51:55 +0300

    OperatorUyg1

---
+++ OperatorUygulama1/OpeatorUygulama1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OpeatorUygulama1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kullanıcıdan aldığınız 2 sayının çarpımı ile x, y, z toplamının farkı nedir ?
+
+            int x = 2, y = 5, z = 1;
+
+            Console.Write("İlk sayıyı girin: ");
+            int sayi1 = int.Parse(Console.ReadLine());
+
+            Console.Write("İkinci sayıyı girin: ");
+            int sayi2 = int.Parse(Console.ReadLine());
+
+            int sonuc = (sayi1 * sayi2) - (x + y + z);
+
+            Console.WriteLine("Sonuç :" + sonuc);
+
+
+
+        }
+    }
+}
